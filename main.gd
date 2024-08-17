1ccf29e4b05b4a2d1f89b4e771fcea51acc6affe
ignacio-quera <ignacio.quera@uc.cl>
2024-08-16 23:31:15 -0400

    windows moment

--- main.gd
+++ main.gd
@@ -1,16 +1,26 @@
-extends Node2D
+extends Node
+@export var player_scene: PackedScene
+func new_player(player_num):
+	var Player = player_scene.instantiate()
+	var player_spawn_location = $PlayerPath/PathPathLocation
+	add_child(Player)
+	Player.start(Vector2(player_num*10, player_num*10), player_num)
+	#$Player.start($StartPosition.position+Vector2(player_num*10, player_num*10), player_num)
 
+func new_game(number_of_players):
+	for player in number_of_players:
+		print(player)
+		new_player(player)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
 	pass
+
+
+func _on_hud_start_game():
+	pass # Replace with function body.
 
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+
