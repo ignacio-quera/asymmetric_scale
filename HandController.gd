20f78b5b08a85613284a87a37a1e650feeeec882
kovaxis <martin.andrighetti@gmail.com>
2024-08-17 15:50:32 -0400

    hands v2

--- HandController.gd
+++ HandController.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const Hand = preload("res://hand.gd")
+
 
 var RECORD_DISTANCE: float = 10
 var MAX_RECORD_DIST: int = 200
@@ -7,94 +9,67 @@ var MAX_RECORD_DURATION: float = 0.5
 var DELAY: float = 1
 var COOLDOWN_DURATION: float = 0.5
 
-@export var action_button: String
-@export var is_left: bool
-@export var trail_gradient: Gradient
+enum Dir {LEFT, RIGHT}
 
-enum State {IDLE, RECORDING, WAITING, REPLAYING, COOLDOWN}
-
-var state: State = State.IDLE
-var time: float = 0
-var follow_curve: Curve2D = null
-
+var choosing: bool = false
+var choose_dir: Dir = Dir.LEFT
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Hand/Sprite2D.flip_h = is_left
-	$Line2D.gradient = trail_gradient
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	time += delta
-	match state:
-		State.IDLE:
-			if Input.is_action_pressed(action_button):
-				state = State.RECORDING
-				time = 0
-		State.RECORDING:
-			var pos = get_viewport().get_mouse_position()
-			var n = $Line2D.get_point_count()
-			if n > 1:
-				$Line2D.remove_point(n-1)
-			n = $Line2D.get_point_count()
-			if n > 0 and ($Line2D.get_point_position(n-1) - pos).length() >= RECORD_DISTANCE:
-				$Line2D.add_point(pos)
-			$Line2D.add_point(pos)
-			var total_dist = 0
-			for i in range(1, $Line2D.get_point_count()):
-				var a = $Line2D.get_point_position(i-1)
-				var b = $Line2D.get_point_position(i)
-				total_dist += (b - a).length()
-			if (time >= MAX_RECORD_DURATION
-					or total_dist >= MAX_RECORD_DIST
-					or not Input.is_action_pressed(action_button)):
-				if n <= 2:
-					state = State.IDLE
-					time = 0
-					$Line2D.clear_points()
+	var pos = get_viewport().get_mouse_position()
+	var want_l = Input.is_action_pressed("drag_left") and $HandL.ready_to_attack()
+	var want_r = Input.is_action_pressed("drag_right") and $HandR.ready_to_attack()
+	var want_choose = want_l or want_r
+	if want_choose and not choosing:
+		$Anchor.position = pos
+		if want_l:
+			choose_dir = Dir.LEFT
+		else:
+			choose_dir = Dir.RIGHT
+	var action = null
+	var d = pos - $Anchor.position
+	if d.length() < 30:
+		action = Hand.Action.FIST
+	elif abs(d.x) > abs(d.y):
+		if (d.x < 0) == (choose_dir == Dir.RIGHT):
+			action = Hand.Action.SWIPE
+		else:
+			action = Hand.Action.BACKHAND
+	else:
+		if d.y < 0:
+			action = Hand.Action.PUNCH
+		else:
+			action = Hand.Action.CLAW
+	if not want_choose and choosing:
+		var hand: Hand = ($HandL if choose_dir == Dir.LEFT else $HandR)
+		hand.do_action(action, $Anchor.position)
+	choosing = want_choose
+	$Anchor.visible = choosing
+	var icon = null
+	if choosing:
+		match action:
+			Hand.Action.FIST:
+				icon = preload("res://assets/images/bigfellasprites/icon_fist.png")
+			Hand.Action.SWIPE:
+				if choose_dir == Dir.RIGHT:
+					icon = preload("res://assets/images/bigfellasprites/icon_swipe.png")
 				else:
-					state = State.WAITING
-					time -= MAX_RECORD_DURATION
-					follow_curve = Curve2D.new()
-					follow_curve.add_point($Hand.position, Vector2.ZERO, $Line2D.get_point_position(0) - $Hand.position)
-					follow_curve.add_point($Line2D.get_point_position(0), Vector2.UP*100)
-		State.WAITING:
-			if time >= DELAY:
-				state = State.REPLAYING
-				time = 0
-				follow_curve = Curve2D.new()
-				var n = $Line2D.get_point_count()
-				for i in range(n):
-					var out = Vector2.ZERO
-					if i == n-1:
-						out = Vector2.UP * 20
-					follow_curve.add_point($Line2D.get_point_position(i), Vector2.ZERO, out)
-			else:
-				var t = time / DELAY
-				$Hand.position = follow_curve.samplef(clamp(t, 0, 1))
-		State.REPLAYING:
-			var t = time * 500
-			t = t / follow_curve.get_baked_length()
-			if t <= 1:
-				var want_n = follow_curve.point_count - floor(t * follow_curve.point_count)
-				while want_n < $Line2D.get_point_count():
-					$Line2D.remove_point(0)
-				var pos = follow_curve.sample_baked(t * follow_curve.get_baked_length())
-				$Hand.position = pos
-			else:
-				#var last = follow_curve.get_point_position(follow_curve.point_count-1)
-				#follow_curve = Curve2D.new()
-				#follow_curve.add_point(last)
-				#follow_curve.add_point(last+Vector2.UP*4)
-				$Line2D.clear_points()
-				state = State.COOLDOWN
-				time = 0
-		State.COOLDOWN:
-			if time >= COOLDOWN_DURATION:
-				state = State.IDLE
-				time = 0
-			else:
-				var t = time / COOLDOWN_DURATION
-				$Hand.position = follow_curve.sample(follow_curve.point_count, t)
-	$Hand/CollisionShape2D.disabled = (state != State.REPLAYING)
+					icon = preload("res://assets/images/bigfellasprites/icon_swipe_r.png")
+			Hand.Action.BACKHAND:
+				if choose_dir == Dir.RIGHT:
+					icon = preload("res://assets/images/bigfellasprites/icon_swipe_r.png")
+				else:
+					icon = preload("res://assets/images/bigfellasprites/icon_swipe.png")
+			Hand.Action.CLAW:
+				icon = preload("res://assets/images/bigfellasprites/icon_claw.png")
+			Hand.Action.PUNCH:
+				icon = preload("res://assets/images/bigfellasprites/icon_punch.png")
+	if icon == null:
+		Input.set_custom_mouse_cursor(null)
+	else:
+		Input.set_custom_mouse_cursor(icon, 0, icon.get_size()/2)
--- hand.gd
+++ hand.gd
@@ -1,14 +1,58 @@
 extends Area2D
 
 
+@export var is_left: bool
+
+const SETUP_TIME: float = 1
+
+enum Action {SWIPE, BACKHAND, FIST, CLAW, PUNCH}
+
+var follow_curve: Curve2D = null
+var time: float = 0
+var max_time: float = 0
+var deadly: bool = false
+
+func ready_to_attack():
+	return follow_curve == null
+
+func do_action(act: Action, pos: Vector2):
+	var scrsize = get_viewport_rect().size
+	match act:
+		Action.FIST:
+			follow_curve = Curve2D.new()
+			follow_curve.add_point(position)
+			follow_curve.add_point(pos + Vector2.UP*100)
+			follow_curve.add_point(pos, Vector2.UP*100)
+			time = -SETUP_TIME
+			max_time = 1
+		Action.SWIPE:
+			var forward = (1 if is_left else -1)
+			follow_curve = Curve2D.new()
+			follow_curve.add_point(position)
+			follow_curve.add_point(Vector2(scrsize.x*(1-forward)/2, pos.y))
+			follow_curve.add_point(Vector2(scrsize.x*(1+forward)/2+100*forward, pos.y), Vector2.LEFT*100*forward)
+			time = -SETUP_TIME
+			max_time = 1
+			deadly = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Sprite2D.flip_h = is_left
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+func _process(delta):
+	time += delta
+	if time >= max_time:
+		time = 0
+		follow_curve = null
+		deadly = false
+	$CollisionShape2D.disabled = not deadly or time < 0
+	if follow_curve != null:
+		if time < 0:
+			position = follow_curve.sample(0, 1 + time / SETUP_TIME)
+		else:
+			position = follow_curve.samplef(1 + time / max_time * (follow_curve.point_count - 1))
 
 
 func _on_body_entered(body):
