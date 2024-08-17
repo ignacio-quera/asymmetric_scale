012473cf245f43feb3bc7263061332cafeb64f6b
viceibacache <vibacach@uc.cl>
2024-08-17 01:00:42 -0400

    linux moment

--- main.gd
+++ main.gd
@@ -5,7 +5,7 @@ func new_player(player_num):
 	var player_spawn_location = $PlayerPath/PathPathLocation
 	player_spawn_location.progress_ratio = player_num / 4
 	add_child(Player)
-	Player.start(player_spawn_location.position, player_num)
+	Player.start(player_spawn_location.global_position, player_num)
 	#$Player.start($StartPosition.position+Vector2(player_num*10, player_num*10), player_num)
 
 func new_game(number_of_players):
