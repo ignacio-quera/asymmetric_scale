f41def15d856082d4f6532ddc711c3c6e42d0645
kovaxis <martin.andrighetti@gmail.com>
2024-08-17 18:56:57 -0400

    hands improvement

--- HandController.gd
+++ HandController.gd
@@ -2,17 +2,24 @@ extends Node
 
 const Hand = preload("res://hand.gd")
 
+const HOVER_GRADIENT = [preload("res://assets/gradients/lefthover.tres"), preload("res://assets/gradients/righthover.tres")]
+var SINGLE_GRADIENT = HOVER_GRADIENT.duplicate()
 
-var RECORD_DISTANCE: float = 10
-var MAX_RECORD_DIST: int = 200
-var MAX_RECORD_DURATION: float = 0.5
-var DELAY: float = 1
-var COOLDOWN_DURATION: float = 0.5
+const ANCHOR_READY = preload("res://assets/images/bigfellasprites/anchor_ready.png")
+const ANCHOR_WAIT = preload("res://assets/images/bigfellasprites/anchor_cooldown.png")
+
+const FIST_DISTANCE: float = 30
+const DELAY: float = 1
+const COOLDOWN_DURATION: float = 0.5
 
 enum Dir {LEFT, RIGHT}
 
 var choosing: bool = false
 var choose_dir: Dir = Dir.LEFT
+
+func _init():
+	for grad in SINGLE_GRADIENT:
+		grad.remove_point(1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,9 +28,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var scr = get_viewport().get_visible_rect().size
 	var pos = get_viewport().get_mouse_position()
-	var want_l = Input.is_action_pressed("drag_left") and $HandL.ready_to_attack()
-	var want_r = Input.is_action_pressed("drag_right") and $HandR.ready_to_attack()
+	var want_l = Input.is_action_pressed("drag_left")
+	var want_r = Input.is_action_pressed("drag_right")
 	var want_choose = want_l or want_r
 	if want_choose and not choosing:
 		$Anchor.position = pos
@@ -31,9 +39,10 @@ func _process(delta):
 			choose_dir = Dir.LEFT
 		else:
 			choose_dir = Dir.RIGHT
+	var anchor = $Anchor.position
 	var action = null
-	var d = pos - $Anchor.position
-	if d.length() < 30:
+	var d = pos - anchor
+	if d.length() < FIST_DISTANCE:
 		action = Hand.Action.FIST
 	elif abs(d.x) > abs(d.y):
 		if (d.x < 0) == (choose_dir == Dir.RIGHT):
@@ -45,29 +54,46 @@ func _process(delta):
 			action = Hand.Action.PUNCH
 		else:
 			action = Hand.Action.CLAW
-	if not want_choose and choosing:
-		var hand: Hand = ($HandL if choose_dir == Dir.LEFT else $HandR)
-		hand.do_action(action, $Anchor.position)
+	var hand: Hand = ($HandL if choose_dir == Dir.LEFT else $HandR)
+	if not want_choose and choosing and hand.ready_to_attack():
+		hand.do_action(action, anchor)
 	choosing = want_choose
 	$Anchor.visible = choosing
+	$Anchor.texture = (ANCHOR_READY if hand.ready_to_attack() else ANCHOR_WAIT)
+	$ActionPreview.visible = choosing and hand.ready_to_attack()
 	var icon = null
 	if choosing:
+		$ActionPreview.clear_points()
+		$ActionPreview.width = 2*$HandL/CollisionShape2D.shape.radius
+		$ActionPreview.gradient = HOVER_GRADIENT[choose_dir]
 		match action:
 			Hand.Action.FIST:
 				icon = preload("res://assets/images/bigfellasprites/icon_fist.png")
+				$ActionPreview.add_point(anchor)
+				$ActionPreview.add_point(anchor+Vector2.UP)
+				$ActionPreview.gradient = SINGLE_GRADIENT[choose_dir]
+				$ActionPreview.width = 2*FIST_DISTANCE
 			Hand.Action.SWIPE:
+				$ActionPreview.add_point(Vector2(0, anchor.y))
+				$ActionPreview.add_point(Vector2(scr.x, anchor.y))
 				if choose_dir == Dir.RIGHT:
 					icon = preload("res://assets/images/bigfellasprites/icon_swipe.png")
 				else:
 					icon = preload("res://assets/images/bigfellasprites/icon_swipe_r.png")
 			Hand.Action.BACKHAND:
+				$ActionPreview.add_point(Vector2(scr.x, anchor.y))
+				$ActionPreview.add_point(Vector2(0, anchor.y))
 				if choose_dir == Dir.RIGHT:
 					icon = preload("res://assets/images/bigfellasprites/icon_swipe_r.png")
 				else:
 					icon = preload("res://assets/images/bigfellasprites/icon_swipe.png")
 			Hand.Action.CLAW:
+				$ActionPreview.add_point(Vector2(anchor.x, 0))
+				$ActionPreview.add_point(Vector2(anchor.x, scr.y))
 				icon = preload("res://assets/images/bigfellasprites/icon_claw.png")
 			Hand.Action.PUNCH:
+				$ActionPreview.add_point(Vector2(anchor.x, scr.y))
+				$ActionPreview.add_point(Vector2(anchor.x, 0))
 				icon = preload("res://assets/images/bigfellasprites/icon_punch.png")
 	if icon == null:
 		Input.set_custom_mouse_cursor(null)
--- hand.gd
+++ hand.gd
@@ -2,57 +2,89 @@ extends Area2D
 
 
 @export var is_left: bool
+@export var recover_pos: Marker2D
+@export var hand_texture: Texture2D
 
 const SETUP_TIME: float = 1
+const RECOVER_TIME: float = 1
+const RECOVER_GRACE: float = 0.7
 
 enum Action {SWIPE, BACKHAND, FIST, CLAW, PUNCH}
 
 var follow_curve: Curve2D = null
+var recovering: bool = false
 var time: float = 0
 var max_time: float = 0
 var deadly: bool = false
 
 func ready_to_attack():
-	return follow_curve == null
+	return follow_curve == null or (recovering and time >= max_time - RECOVER_GRACE)
 
 func do_action(act: Action, pos: Vector2):
 	var scrsize = get_viewport_rect().size
+	var forward = (1 if is_left else -1)
+	const MARGIN = 70
+	time = -SETUP_TIME
+	follow_curve = Curve2D.new()
+	follow_curve.add_point(position)
+	recovering = false
 	match act:
 		Action.FIST:
-			follow_curve = Curve2D.new()
-			follow_curve.add_point(position)
 			follow_curve.add_point(pos + Vector2.UP*100)
 			follow_curve.add_point(pos, Vector2.UP*100)
-			time = -SETUP_TIME
 			max_time = 1
 		Action.SWIPE:
-			var forward = (1 if is_left else -1)
-			follow_curve = Curve2D.new()
-			follow_curve.add_point(position)
 			follow_curve.add_point(Vector2(scrsize.x*(1-forward)/2, pos.y))
-			follow_curve.add_point(Vector2(scrsize.x*(1+forward)/2+100*forward, pos.y), Vector2.LEFT*100*forward)
-			time = -SETUP_TIME
+			follow_curve.add_point(Vector2(scrsize.x*(1+forward)/2+MARGIN*forward, pos.y), Vector2.LEFT*100*forward)
+			max_time = 1
+			deadly = true
+		Action.BACKHAND:
+			follow_curve.add_point(Vector2(scrsize.x*(1+forward)/2, pos.y))
+			follow_curve.add_point(Vector2(scrsize.x*(1-forward)/2-MARGIN*forward, pos.y), -Vector2.LEFT*100*forward)
+			max_time = 2
+			deadly = true
+		Action.CLAW:
+			follow_curve.add_point(Vector2(pos.x, 0))
+			follow_curve.add_point(Vector2(pos.x, scrsize.y+MARGIN), Vector2.UP*100)
+			max_time = 1
+			deadly = true
+		Action.PUNCH:
+			follow_curve.add_point(Vector2(pos.x, scrsize.y))
+			follow_curve.add_point(Vector2(pos.x, -MARGIN), -Vector2.UP*100)
 			max_time = 1
 			deadly = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Sprite2D.flip_h = is_left
+	$Sprite2D.texture = hand_texture
+	if is_left:
+		scale.x *= -1
+	position = recover_pos.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	time += delta
-	if time >= max_time:
-		time = 0
-		follow_curve = null
-		deadly = false
-	$CollisionShape2D.disabled = not deadly or time < 0
 	if follow_curve != null:
+		time += delta
 		if time < 0:
 			position = follow_curve.sample(0, 1 + time / SETUP_TIME)
 		else:
-			position = follow_curve.samplef(1 + time / max_time * (follow_curve.point_count - 1))
+			var n = follow_curve.point_count - 1
+			position = follow_curve.samplef(1 + time / max_time * n)
+		if time >= max_time:
+			if recovering:
+				follow_curve = null
+				recovering = false
+			else:
+				follow_curve = Curve2D.new()
+				follow_curve.add_point(position)
+				follow_curve.add_point(position)
+				follow_curve.add_point(recover_pos.position)
+				time = 0
+				max_time = RECOVER_TIME
+				recovering = true
+				deadly = false
+	$CollisionShape2D.disabled = not deadly or time < 0
 
 
 func _on_body_entered(body):
