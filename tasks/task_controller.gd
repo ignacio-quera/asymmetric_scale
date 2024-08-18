fca54863524b99a084dc6694ed01bf1d1f155489
ignacio-quera <ignacio.quera@uc.cl>
2024-08-18 00:30:18 -0400

    item setter wip

--- tasks/task_controller.gd
+++ tasks/task_controller.gd
@@ -3,17 +3,14 @@ extends Node2D
 @export var magic_circle: PackedScene
 
 func _ready():
-	_on_task_spawn_timer_timeout()
 	$task_spawn_timer.start()
-	
-	var task_spawn_timer = get_node("task_spawn_timer")
-	task_spawn_timer.timeout.connect(_on_task_spawn_timer_timeout)
 
 func _on_task_spawn_timer_timeout():
 	print(magic_circle)
-	#var task = magic_circle.instantiate()
-	#task.position = get_spawn_position()
-	#add_child(task)
+	var task = magic_circle.instantiate()
+	task.position = get_spawn_position()
+	add_child(task)
+	task.start()
 
 func get_spawn_position():
 	var centerpos = $task_spawn_area/CollisionShape2D.position + $task_spawn_area.position
